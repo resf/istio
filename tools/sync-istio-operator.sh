6e5194a0f008a82b2c324e11be9a34c5134b381c
Morlay <morlay.null@gmail.com>
2020-12-10 10:05:48 +0800

    fix istio-operator

--- tools/sync-istio-operator.sh
+++ tools/sync-istio-operator.sh
@@ -4,9 +4,11 @@ ISIO_BRANCE=release-1.8
 
 git clone --depth=1 -b ${ISIO_BRANCE} https://github.com/istio/istio.git /tmp/istio
 
-rm -rf charts/istio-operator
+rm -rf charts/istio-operator/templates/*
+rm -rf charts/istio-operator/crds/*
 
-cp -r /tmp/istio/manifests/charts/istio-operator charts/istio-operator
+cp -r /tmp/istio/manifests/charts/istio-operator/templates/* charts/istio-operator/templates/
+cp -r /tmp/istio/manifests/charts/istio-operator/crds/* charts/istio-operator/crds/
+rm charts/istio-operator/templates/namespace.yaml
 
-rm -rf charts/istio-operator/files
 rm -rf /tmp/istio
